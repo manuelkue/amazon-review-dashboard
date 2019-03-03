3e81cc28850f97ea1687470f71393c59169434f3
Manuel Küfeldt <git@kuefeldt.de>
2019-03-03 01:27:31 +0100

    Fetch all reviews and the profile. Send it via ipcMain to frontend. Error handling, closing connections

--- electron-main.js
+++ electron-main.js
@@ -1,19 +1,33 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const puppeteer = require('puppeteer');
 let win
+
+const maxScrapingTime = 10000;
+let scraping;
 
 ////////////////////////
 // Puppeteer
 ////////////////////////
 
-crawlReviewIDs('https://www.amazon.de/gp/profile/amzn1.account.AH66ZZWKVCLVHB7XDYDHHE3GYK2A')
+ipcMain.on('startCrawl', (event, arg) => {
+  console.log(arg)
+  crawlReviews('https://www.amazon.de/gp/profile/amzn1.account.AHUDYEBSEIX6KAE3Q3NNKR5PAFVQ')
+})
 
-async function crawlReviewIDs(userProfileURL){
+
+async function crawlReviews(userProfileURL){
   const scrapeStartTime = new Date().getTime()
+  scraping = true;
+  let profile = {};
   let reviews = [];
 
-  const browser = await puppeteer.launch({ headless: true })
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
+  setTimeout(async () => {
+    await closeConnection (page, browser)
+    scraping && win.webContents.send('scrapeError', 'Scraping interrupted, Scraping took too long')
+  }, maxScrapingTime)
+
   await page.setViewport({ width: 500, height: 10000 });
 
   //Filter for relevant files & fetch json-Data
@@ -32,38 +46,65 @@ async function crawlReviewIDs(userProfileURL){
   page.on('response', response => {
     if(response.url().includes('profilewidget')){
       response.text()
-      .then(json => {
-        let reviewCluster = JSON.parse(json);
+      .then(async json => {
+        const reviewCluster = JSON.parse(json);
         reviews.push(...reviewCluster['contributions']);
         console.log("reviewsCount", reviews.length);
         if(!reviewCluster.nextPageToken){
-          console.log("\n#############\nReached End of Page");
+          console.log("\n#############\nScrapeComplete");
           console.log("Total time of scraping", new Date().getTime() - scrapeStartTime, "ms")
           console.log("reviewsCount", reviews.length);
+          scraping = false;
+          win.webContents.send('scrapeComplete', reviews)
+          await closeConnection (page, browser)
+        }else{
+          page.evaluate(() => {
+            window.scrollBy(0, window.innerHeight)
+          });
         }
-        page.evaluate(() => {
-          window.scrollBy(0, window.innerHeight);
-        });
 
       })
-      .catch(err => console.error(err))
+      .catch(err => {
+        blockedByAmazon(err, page, browser)
+      })
+    }
+    if(response.url().includes('gamification')){
+      response.text()
+      .then(async json => {
+        profile = Object.assign(profile, JSON.parse(json))
+      })
+      .catch(err => {
+        blockedByAmazon(err, page, browser)
+      })
     }
   })
 
   await page.goto(userProfileURL)
-
-  try{
-    const helpfulVotes = await page.$eval('.dashboard-desktop-stat-value', el => el.innerText)
-    console.log("Helpful Votes", helpfulVotes)
-  }
-  catch(err){
-    console.error(err)
-  }
-
+  const name = await page.$eval('.name-container span', el => el.innerText)
+  const rank = await page.$eval('.a-spacing-base a.a-link-normal', el => +el.getAttribute('href').split('rank=')[1].split('#')[0])
+  console.log(rank);
+  profile = Object.assign(profile, {name, rank})
+  win.webContents.send('profileScraped', profile)
   console.log("First full load after", new Date().getTime() - scrapeStartTime, "ms")
+}
 
-  await page.close();
-  await browser.close()
+async function blockedByAmazon(err, page, browser){
+  win.webContents.send('scrapeError', 'Blocked by Amazon, too many attempts')
+  scraping = false;
+  await closeConnection (page, browser)
+  console.error(err)
+}
+
+async function closeConnection (page, browser){
+    setTimeout(async () => {
+      try{
+        await page.close();
+        await browser.close();
+      }
+      catch{
+        console.log("connection already closed")
+      }
+    }, 2000)
 }
 
 ////////////////////////
@@ -72,7 +113,7 @@ async function crawlReviewIDs(userProfileURL){
 
 
 function createWindow () {
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1600, height: 1000 })
   win.loadFile('build/index.html')
   win.webContents.openDevTools()
 
--- src/components/ReviewsListComponent.js
+++ src/components/ReviewsListComponent.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react"
 import reviewsData from "../data/Reviews";
 import ReviewItemComponent from "./ReviewItemComponent";
+const { ipcRenderer } = window.require('electron')
 
 class ReviewsListComponent extends Component{
     constructor() {
         super()
         this.state = {
-            reviewsBy: "Manuel",
-            helpfulVotes: "1.400"
+            scrapeStatus: null,
+            helpfulVotes: null,
+            reviesCount: null,
+            reviewsBy: "Manuel"
         }
+        
+        setTimeout(() => {
+            console.log("starting crawl")
+            ipcRenderer.send('startCrawl', 'starting Crawl')
+            this.setState({
+                scrapeStatus: 'Scraping...'
+            })
+        }, 1000)
     }
 
     render() {
@@ -16,10 +27,35 @@ class ReviewsListComponent extends Component{
             <ReviewItemComponent key={review.id} review={review} />
         )
 
+        ipcRenderer.on('scraping', (event, reviews) => {
+            console.log(reviews)
+        })
+        ipcRenderer.on('profileScraped', (event, profile) => {
+            console.log("UserProfile", profile)
+            this.setState({
+                helpfulVotes: profile.helpfulVotes.helpfulVotesData.count,
+                reviesCount: profile.reviews.reviewsCountData.count
+            })
+        })
+        ipcRenderer.on('scrapeComplete', (event, reviews) => {
+            console.log("Scrape complete", reviews)
+            this.setState({
+                scrapeStatus: 'Scraping completed'
+            })
+        })
+        ipcRenderer.on('scrapeError', (event, message) => {
+            console.error(message)
+            this.setState({
+                scrapeStatus: message
+            })
+        })
+
         return (
             <div className="reviews-list">
-                <p>Reviews of {this.state.reviewsBy}</p>
-                <p>Helpful votes {this.state.helpfulVotes}</p>
+                <p>Status: {this.state.scrapeStatus}</p>
+                <p>Helpful votes: {this.state.helpfulVotes}</p>
+                <p>Reviews: {this.state.reviesCount}</p>
+                <p>Author: {this.state.reviewsBy}</p>
                 <form>
                     {reviewsComponents}
                 </form>
