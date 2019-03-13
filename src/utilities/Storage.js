731ab538aeee4ba068870698f79ee38af19b3d96
Manuel Küfeldt <git@kuefeldt.de>
2019-03-13 01:37:34 +0100

    Changed storage writing functions from async -> sync so errors / emptying of files don't occurr anymore

--- src/utilities/Storage.js
+++ src/utilities/Storage.js
@@ -1,6 +1,5 @@
 const electron = window.require("electron");
 const path = window.require("path");
-const fs = window.require("fs").promises;
 const fsSync = window.require("fs");
 
 const storageDir = "JSONStorage";
@@ -43,35 +42,29 @@ export class Storage {
 
   // ...and this will set it
   async set(key, val) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {         
+       //@TODO: If new write action should start before the current one is not finished add to line that is processed after the current write action is finished -> File won't be destroyed if current writing process gets interrupted
       parseDataFile(this.path, this.opts.defaults)
-        .then(async file => {
+        .then(file => {
           file[key] = val;
           // Wait, I thought using the node.js' synchronous APIs was bad form?
           // We're not writing a server so there's not nearly the same IO demand on the process
           // Also if we used an async API and our app was quit before the asynchronous write had a chance to complete,
-          // we might lose that data. Note that in a real app, we would try/catch this.
+          // we might lose that data. Tested with async -> Doesn't work often: 
+          // https://stackoverflow.com/questions/30886217/node-js-fs-writefile-empties-the-file
 
           //@TODO: Eventuell einbauen, dass er die alte Datei sichert und bei einem Schreibfehler diese alte Datei wieder darüber schreibt, um keine Daten zu verlieren
 
-          //@TODO: If new write action should start before the current one is not finished add to line that is processed after the current write action is finished -> File won't be destroyed if current writing process gets interrupted
-
           console.log("try to save", file, "to", this.path);
-          fs.writeFile(this.path, JSON.stringify(file), 'utf8', (err) => {
-            if(err) {
-                console.error(err);
-                reject(err)
-            }
-            console.log("File saved successfully!");
-          })
-          .then(() => {
+          try{
+            fsSync.writeFileSync(this.path, JSON.stringify(file), 'utf8')
             console.log("saved", file, "to", this.path);
             resolve(true);
-          })
-          .catch(err => {
-              console.error(err)
-              reject(err)
-            });
+          }
+          catch(err){
+            console.error(err);
+            reject(err)
+          }
         })
         .catch(err => reject(err));
     });
