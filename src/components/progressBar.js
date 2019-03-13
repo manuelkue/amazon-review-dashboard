68cab6fbba02b8e727d447334dfe30a97231fca9
Manuel Küfeldt <git@kuefeldt.de>
2019-03-14 00:31:50 +0100

    progressBar inlineStyling

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -1,9 +1,26 @@
 import React from "react"
 
 export const ProgressBar = ({progress}) => {
+
+    const wrapperStyle={
+        width: '100%',
+        boxSizing:'border-box',
+        background: 'var(--color-light2)',
+        borderRadius:'var(--border-radius-m)',
+        marginBottom: 'var(--spacing-s)'
+    }
+
+    const innerStyle={
+        width: progress + '%',
+        display: 'block',
+        height:'var(--spacing-s)',
+        background: 'var(--color-secondary)',
+        borderRadius:'var(--border-radius-m)'
+    }
+
     return(
-    <div className="progressBar" style={{width: '100%', boxSizing:'border-box',  background: 'var(--color-light2)', borderRadius:'var(--border-radius-m)', marginBottom: 'var(--spacing-s)'}}>
-        <div style={{width: progress + '%', display: 'block', height:'var(--spacing-s)', background: 'var(--color-secondary)', borderRadius:'var(--border-radius-m)'}}></div>
+    <div className="progressBar" style={wrapperStyle}>
+        <div style={innerStyle}></div>
     </div>
     )
 }
