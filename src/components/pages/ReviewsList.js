3b697a7748ad3472fc5562434f6b3487178de107
Manuel Küfeldt <git@kuefeldt.de>
2019-03-30 13:21:46 +0100

    changes reviewsList from functional to class component to dynamically reload items

--- src/components/pages/ReviewsList.js
+++ src/components/pages/ReviewsList.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Component} from "react"
 import ReviewItem from "../ReviewItem";
 
 import {methods} from "../../utilities/methods";
@@ -13,44 +13,52 @@ import { ProgressBar } from "../progressBar";
 
 //@TODO: integrate sort by clicking the header, sort for deleted as well
 
-export const ReviewsList = ({reviews, config, status, reviewFunctions})  => {
+export class ReviewsList extends Component {
 
-    if(methods.fetchURLData(config.fetchURL)){
-        let filteredReviews = [...reviews.filter(review => config.fetchURL.includes(review.userId))]
-        methods.sortObjectArray(filteredReviews, config.sortReviewsBy, config.sortReviewsAscending)
-        const reviewsComponents = [...filteredReviews]
-            .map(review => 
-                <ReviewItem key={review.externalId} review={review} reviewFunctions={reviewFunctions} />
+    constructor(props){
+        super()
+    }
+
+    render(){
+        const {reviews, config, status, reviewFunctions} = this.props
+        
+        if(methods.fetchURLData(config.fetchURL)){
+            let filteredReviews = [...reviews.filter(review => config.fetchURL.includes(review.userId))]
+            methods.sortObjectArray(filteredReviews, config.sortReviewsBy, config.sortReviewsAscending)
+            const reviewsComponents = [...filteredReviews]
+                .map(review => 
+                    <ReviewItem key={review.externalId} review={review} reviewFunctions={reviewFunctions} />
+                )
+                
+            return (
+                <div className="reviews-list">
+                    <h1>Reviews</h1>
+                    <div className="reviewItem reviewsHeader">
+                        <div><i className="material-icons">open_in_new</i></div>
+                        <div>Product</div>
+                        <div>Review Title</div>
+                        <div><i className="material-icons" style={{fontSize : '16px'}}>star_half</i></div>
+                        <div><i className="material-icons" style={{fontSize : '16px'}}>star</i></div>
+                        <div><i className="material-icons" style={{fontSize : '16px'}}>thumb_up</i></div>
+                        <div>Date</div>
+                    </div>
+                    <ProgressBar progress={status.scrapeProgress}></ProgressBar>
+                    <div className="reviewItemsWrapper">
+                        {reviewsComponents}
+                        {!reviewsComponents.length && 
+                            <div className="reviewItem review-notification"><span>Reviews loaded: {status.scrapeProgress}%</span></div>
+                        }
+                    </div>
+                </div>
             )
-            
-        return (
-            <div className="reviews-list">
-                <h1>Reviews</h1>
-                <div className="reviewItem reviewsHeader">
-                    <div><i className="material-icons">open_in_new</i></div>
-                    <div>Product</div>
-                    <div>Review Title</div>
-                    <div><i className="material-icons" style={{fontSize : '16px'}}>star_half</i></div>
-                    <div><i className="material-icons" style={{fontSize : '16px'}}>star</i></div>
-                    <div><i className="material-icons" style={{fontSize : '16px'}}>thumb_up</i></div>
-                    <div>Date</div>
+        }else{
+            return (
+                <div className="reviews-list">
+                    <h1>Reviews</h1>
+                    <div className="reviewItem review-notification"><span>Please specify a URL in the user area that should be used for fetching review data.</span></div>
                 </div>
-                <ProgressBar progress={status.scrapeProgress}></ProgressBar>
-                <div className="reviewItemsWrapper">
-                    {reviewsComponents}
-                    {!reviewsComponents.length && 
-                        <div className="reviewItem review-notification"><span>Reviews loaded: {status.scrapeProgress}%</span></div>
-                    }
-                </div>
-            </div>
-        )
-    }else{
-        return (
-            <div className="reviews-list">
-                <h1>Reviews</h1>
-                <div className="reviewItem review-notification"><span>Please specify a URL in the user area that should be used for fetching review data.</span></div>
-            </div>
-        )
+            )
+        }
     }
 
 }
