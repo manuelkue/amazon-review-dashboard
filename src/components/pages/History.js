32a1acdfbe312a45bc6adf6c2d4f28ca6466d3bf
Manuel Küfeldt <git@kuefeldt.de>
2019-04-01 08:06:52 +0200

    Changed classComponents History / ReviewsList to functional components with hooks (useState, useEffect)

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -5,71 +5,54 @@ import { InfinitLoadingSentinel } from "../InfinitLoadingSentinel";
 
 import {methods} from "../../utilities/methods";
 
-export class History extends Component{
+export const History = ({config, status, reviews}) => {
 
-    constructor(props){
-        super()
+    // limit shown number at the start of the component
+    const [loadedHistoryItemsCount, setLoadedHistoryItemsCount] = useState(30)
 
-        // limit shown number at the start of the component
-        this.state = {
-            loadedHistoryItemsCount : 30
-        }
-    }
+    useEffect(() => {
+        showMoreHistoryItems()
+    }, [])
 
-    render(){
-        const {config, status, reviews} = this.props
-        
-        if(methods.fetchURLData(config.fetchURL)){
-            let loadedReviews = reviews;
-            //Sort by last syncTimestamp
-            methods.sortObjectArray(loadedReviews, 'syncTimestamp', false)
-
-            const historyComponents =
-                loadedReviews.filter(review => config.fetchURL.includes(review.userId) && review.reviewHistory.length)
-                .slice(0, this.state.loadedHistoryItemsCount)
-                .map(review => 
-                    <HistoryItem key={review.externalId} review={review} />
-                )
-                
-            return (
-                <div className="history">
-                    <h1>History</h1>
-                    <ProgressBar progress={status.scrapeProgress}></ProgressBar>
-                    {!historyComponents.length && 
-                        <div className="reviewItem review-notification"><span>Reviews loaded: {status.scrapeProgress}%</span></div>
-                    }
-                    <div className="historyItemWrapper">
-                        {historyComponents}
-                    </div>
-                    {!!historyComponents.length && 
-                        <InfinitLoadingSentinel actionOnIntersecting={this.showMoreHistoryItems.bind(this)} distanceToBottom={200} />
-                    }
-                </div>
-            )
-        }else{
-            return (
-                <div className="history">
-                    <h1>History</h1>
-                    <div className="reviewItem review-notification"><span>Please specify a URL in the user area that should be used for fetching review data.</span></div>
-                </div>
-            )
-        }
-    }
-
-    showMoreHistoryItems(){
-        this.setState({
-            loadedHistoryItemsCount: this.state.loadedHistoryItemsCount + 1000
-        })
+    const showMoreHistoryItems = () => {
+        // Increase the number of displayed reviews to higher amount.
+        setLoadedHistoryItemsCount(loadedHistoryItemsCount + 1000)
         console.log("More HistoryItems loaded");
     }
+    
+    if(methods.fetchURLData(config.fetchURL)){
+        let loadedReviews = reviews;
+        //Sort by last syncTimestamp
+        methods.sortObjectArray(loadedReviews, 'syncTimestamp', false)
 
-    componentDidMount(){
-        // Increase the number of displayed reviews to higher amount.
-        //@TODO: Find out why this has to happen in a Timeout. Without, the render() at start waits for the setState...
-        setTimeout(() => {
-            this.setState({
-                loadedHistoryItemsCount: 1000
-            })
-        }, 0);
+        const historyComponents =
+            loadedReviews.filter(review => config.fetchURL.includes(review.userId) && review.reviewHistory.length)
+            .slice(0, loadedHistoryItemsCount)
+            .map(review => 
+                <HistoryItem key={review.externalId} review={review} />
+            )
+            
+        return (
+            <div className="history">
+                <h1>History</h1>
+                <ProgressBar progress={status.scrapeProgress}></ProgressBar>
+                {!historyComponents.length && 
+                    <div className="reviewItem review-notification"><span>Reviews loaded: {status.scrapeProgress}%</span></div>
+                }
+                <div className="historyItemWrapper">
+                    {historyComponents}
+                </div>
+                {!!historyComponents.length && 
+                    <InfinitLoadingSentinel actionOnIntersecting={showMoreHistoryItems} distanceToBottom={200} />
+                }
+            </div>
+        )
+    }else{
+        return (
+            <div className="history">
+                <h1>History</h1>
+                <div className="reviewItem review-notification"><span>Please specify a URL in the user area that should be used for fetching review data.</span></div>
+            </div>
+        )
     }
 }
--- src/components/pages/ReviewsList.js
+++ src/components/pages/ReviewsList.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react"
+import React, {Component, useState, useEffect} from "react"
 import ReviewItem from "../ReviewItem";
 
 import {methods} from "../../utilities/methods";
@@ -7,84 +7,68 @@ import { InfinitLoadingSentinel } from "../InfinitLoadingSentinel";
 
 
 // Functional / stateless component, pass props and work directly with it.
-// passed prop can be accessed via destructuring in the building of the comonent
+// passed prop can be accessed via destructuring in the building of the component
 // -> ({reviews, config}) = (props)
+// Utilizes hooks to use "class functionality" (useState, useEffect)
 
 //@TODO: Include search field to filter reviews
 
 //@TODO: integrate sort by clicking the header, sort for deleted as well
 
-export class ReviewsList extends Component {
+export const ReviewsList = ({reviews, config, status, reviewFunctions}) => {
 
-    constructor(props){
-        super()
+    // limit shown number at the start of the component
+    const [loadedReviewsCount, setLoadedReviewsCount] = useState(30)
 
-        // limit shown number at the start of the component
-        this.state = {
-            loadedReviewsCount : 30
-        }
-    }
+    useEffect(() => {
+        showMoreReviews()
+    }, [])
 
-    render(){
-        const {reviews, config, status, reviewFunctions} = this.props
-        
-        if(methods.fetchURLData(config.fetchURL)){
-            let filteredReviews = [...reviews.filter(review => config.fetchURL.includes(review.userId))]
-            methods.sortObjectArray(filteredReviews, config.sortReviewsBy, config.sortReviewsAscending)
-            const reviewsComponents = [...filteredReviews].slice(0, this.state.loadedReviewsCount)
-                .map(review => 
-                    <ReviewItem key={review.externalId} review={review} reviewFunctions={reviewFunctions} />
-                )
-                
-            return (
-                <div className="reviews-list">
-                    <h1>Reviews</h1>
-                    <div className="reviewItem reviewsHeader">
-                        <div className="material-icons" >open_in_new</div>
-                        <div>Product</div>
-                        <div>Review Title</div>
-                        <div className="material-icons" style={{fontSize : '16px'}}>star_half</div>
-                        <div className="material-icons" style={{fontSize : '16px'}}>star</div>
-                        <div className="material-icons" style={{fontSize : '16px'}}>thumb_up</div>
-                        <div>Date</div>
-                    </div>
-                    <ProgressBar progress={status.scrapeProgress}/>
-                    <div className="reviewItemsWrapper">
-                        {!reviewsComponents.length && 
-                            <div className="reviewItem review-notification"><span>Reviews loaded: {status.scrapeProgress}%</span></div>
-                        }
-                        {reviewsComponents}
-                        {!!reviewsComponents.length && 
-                            <InfinitLoadingSentinel actionOnIntersecting={this.showMoreReviews.bind(this)} distanceToBottom={200} />
-                        }
-                    </div>
-                </div>
-            )
-        }else{
-            return (
-                <div className="reviews-list">
-                    <h1>Reviews</h1>
-                    <div className="reviewItem review-notification"><span>Please specify a URL in the user area that should be used for fetching review data.</span></div>
-                </div>
-            )
-        }
-    }
-
-    showMoreReviews(){
-        this.setState({
-            loadedReviewsCount: this.state.loadedReviewsCount + 1000
-        })
+    const showMoreReviews = () => {
+        // Increase the number of displayed reviews to higher amount.
+        setLoadedReviewsCount(loadedReviewsCount + 1000)
         console.log("More Reviews loaded");
     }
 
-    componentDidMount(){
-        // Increase the number of displayed reviews to higher amount.
-        //@TODO: Find out why this has to happen in a Timeout. Without, the render() at start waits for the setState...
-        setTimeout(() => {
-            this.setState({
-                loadedReviewsCount: 1000
-            })
-        }, 0);
+    if(methods.fetchURLData(config.fetchURL)){
+        let filteredReviews = [...reviews.filter(review => config.fetchURL.includes(review.userId))]
+        methods.sortObjectArray(filteredReviews, config.sortReviewsBy, config.sortReviewsAscending)
+        const reviewsComponents = [...filteredReviews].slice(0, loadedReviewsCount)
+            .map(review => 
+                <ReviewItem key={review.externalId} review={review} reviewFunctions={reviewFunctions} />
+            )
+            
+        return (
+            <div className="reviews-list">
+                <h1>Reviews</h1>
+                <div className="reviewItem reviewsHeader">
+                    <div className="material-icons" >open_in_new</div>
+                    <div>Product</div>
+                    <div>Review Title</div>
+                    <div className="material-icons" style={{fontSize : '16px'}}>star_half</div>
+                    <div className="material-icons" style={{fontSize : '16px'}}>star</div>
+                    <div className="material-icons" style={{fontSize : '16px'}}>thumb_up</div>
+                    <div>Date</div>
+                </div>
+                <ProgressBar progress={status.scrapeProgress}/>
+                <div className="reviewItemsWrapper">
+                    {!reviewsComponents.length && 
+                        <div className="reviewItem review-notification"><span>Reviews loaded: {status.scrapeProgress}%</span></div>
+                    }
+                    {reviewsComponents}
+                    {!!reviewsComponents.length && 
+                        <InfinitLoadingSentinel actionOnIntersecting={showMoreReviews} distanceToBottom={200} />
+                    }
+                </div>
+            </div>
+        )
+    }else{
+        return (
+            <div className="reviews-list">
+                <h1>Reviews</h1>
+                <div className="reviewItem review-notification"><span>Please specify a URL in the user area that should be used for fetching review data.</span></div>
+            </div>
+        )
     }
 
 }
