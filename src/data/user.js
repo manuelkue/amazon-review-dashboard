55890d7b3f4ea374c9655fb8e8250624e6610e8a
Manuel Küfeldt <manuel.kuefeldt@bytabo.de>
2019-04-03 21:53:14 +0200

    Deleted data/Reviews, user

--- src/data/user.js
+++
@@ -1,9 +0,0 @@
-const userData = {
-    id:'AHIML2WDUBRNHH47SS5PZEWVBOJA',
-    name: 'Manuel Küfeldt',
-    rank: '129',
-    helpfulVotes: 222,
-    reviewsCount: 800
-}
-
-export default userData
