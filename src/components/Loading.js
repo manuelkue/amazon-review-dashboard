7a6fe6673648cdbd06428911783fd5eefe02b9e0
Manuel Küfeldt <manuel.kuefeldt@bytabo.de>
2019-05-22 17:19:44 +0200

    Added Loading-CSS

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,13 @@
 import React from "react"
 
+const style={
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
 export const Loading = () =>
-    <div>Loading...</div>
+    <div style={style}>Loading...</div>
 
